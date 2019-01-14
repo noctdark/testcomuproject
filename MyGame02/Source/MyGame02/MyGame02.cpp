6e81a3a2fc0bcd826df86713add791417d267369
noctdark <darkchym@gmail.com>
2019-01-14 20:24:27 +0900

    modify

---
+++ MyGame02/Source/MyGame02/MyGame02.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyGame02.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyGame02, "MyGame02" );
